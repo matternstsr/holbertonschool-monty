a0522762f3b8762c632179f95396e9bae3baa373
matternstsr <matternstsr@gmail.com>
2023-08-11 16:33:32 -0700

    all i need is betty

--- allfixedneedsbetty/nop.c
+++
@@ -1,8 +0,0 @@
-#include "monty.h"
-
-void nop(stack_t **stack, unsigned int line_number, int value) {
-    (void)stack;
-    (void)line_number;
-    (void)value;
-    // No operation, do nothing
-}
--- lostsomegainedlittle/free_stack.c
+++
@@ -1,9 +0,0 @@
-#include "monty.h"
-
-void free_stack(stack_t *stack) {
-    while (stack) {
-        stack_t *temp = stack;
-        stack = stack->next;
-        free(temp);
-    }
-}
