e16a9e1d9b07a28f21b5964b9c752c9113c7b18e
matternstsr <matternstsr@gmail.com>
2023-08-14 07:42:38 -0700

    pushed proto changes

--- monty.c
+++ monty.c
@@ -9,20 +9,24 @@
  * @value: Value parameter for some instructions.
  */
 
-/*void ex_in(in_t *inst, st_t *st, char *op, unsigned int line_number, int val)*/
+void ex_in(in_t *inst, st_t *st, char *op, unsigned int line_number, int val)
 
-void execute_instruction(instruction_t *instructions, stack_t **stack, char *opcode, unsigned int line_number, int value) {
-    int i = 0;
-    while (instructions[i].opcode) {
-        if (strcmp(instructions[i].opcode, opcode) == 0) {
-            instructions[i].f(stack, line_number, value);
-            return;
-        }
-        i++;
-    }
-    fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-    free_stack(*stack);
-    exit(EXIT_FAILURE);
+{
+	int i = 0;
+
+	while (inst[i].opcode)
+	{
+		if (strcmp(inst[i].opcode, op) == 0)
+
+		{
+			inst[i].f(&st, line_number, val);
+			return;
+		}
+		i++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, op);
+	free_stack(st);
+	exit(EXIT_FAILURE);
 }
 
 
@@ -34,53 +38,67 @@ void execute_instruction(instruction_t *instructions, stack_t **stack, char *opc
  * Return: EXIT_SUCCESS if successful, EXIT_FAILURE if an error occurs.
  */
 
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	char buffer[1024], *value_str, *opcode;
+	int value;
+	unsigned int line_number;
+	stack_t *stack;
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "USAGE: monty file\n");
-        return EXIT_FAILURE;
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+	stack = NULL;
+	line_number = 1;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+	while (fgets(buffer, sizeof(buffer), file))
+	{
+		opcode = strtok(buffer, " \t\n");
+		if (opcode && opcode[0] != '#')
+		{
+			if (strcmp(opcode, "push") == 0)
+			{
+				value_str = strtok(NULL, " \t\n");
+				if (value_str && is_integer(value_str))
+				{
+					value = atoi(value_str);
+					ex_in(instructions, stack, opcode, line_number, value);
+				}
+				else
+				{
+					fprintf(stderr, "L%d: usage: push integer\n", line_number);
+					free_stack(stack);
+					fclose(file);
+					return (EXIT_FAILURE);
+				}
+			}
+			else
+			{
+				ex_in(instructions, stack, opcode, line_number, 0);
+			}
+		}
+		line_number++;
+	}
 
-    FILE *file = fopen(argv[1], "r");
-    if (!file) {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
-    stack_t *stack = NULL;
-    char buffer[1024];
-    unsigned int line_number = 1;
-    instruction_t instructions[] = {
-        {"push", push},
-        {"pall", pall},
-        {"pint", pint},
-        {"pop", pop},
-        {"swap", swap},
-        {"add", add},
-        {"nop", nop},
-        {NULL, NULL}
-    };
-    while (fgets(buffer, sizeof(buffer), file)) {
-        char *opcode = strtok(buffer, " \t\n");
-        if (opcode && opcode[0] != '#') {
-            if (strcmp(opcode, "push") == 0) {
-                char *value_str = strtok(NULL, " \t\n");
-                if (value_str && is_integer(value_str)) {
-                    int value = atoi(value_str);
-                    execute_instruction(instructions, &stack, opcode, line_number, value);
-                } else {
-                    fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                    free_stack(stack);
-                    fclose(file);
-                    return EXIT_FAILURE;
-                }
-            } else {
-                execute_instruction(instructions, &stack, opcode, line_number, 0);
-            }
-        }
-        line_number++;
-    }
-
-    free_stack(stack);
-    fclose(file);
-    return EXIT_SUCCESS;
+	free_stack(stack);
+	fclose(file);
+	return (EXIT_SUCCESS);
 }
