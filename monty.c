422a84c1ab3a5e4d2e2855c4748a3d9516e613fa
matternstsr <matternstsr@gmail.com>
2023-08-14 20:47:34 -0700

    sending monty.c update

--- monty.c
+++ monty.c
@@ -11,10 +11,13 @@
 
 #include "monty.h"
 
-void execute_instruction(instruction_t *instructions, stack_t **stack, char *opcode, unsigned int line_number, int value) {
+void execute_instruction(instruction_t *instructions, stack_t **stack, char *opcode, unsigned int line_number, int value)
+{
 	int i = 0;
-	while (instructions[i].opcode) {
-		if (strcmp(instructions[i].opcode, opcode) == 0) {
+	while (instructions[i].opcode)
+	{
+		if (strcmp(instructions[i].opcode, opcode) == 0)
+		{
 			instructions[i].f(stack, line_number, value);
 			return;
 		}
@@ -35,14 +38,17 @@ void execute_instruction(instruction_t *instructions, stack_t **stack, char *opc
  */
 
 
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
 		fprintf(stderr, "USAGE: monty file\n");
 		return EXIT_FAILURE;
 	}
 
 	FILE *file = fopen(argv[1], "r");
-	if (!file) {
+	if (!file)
+	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		return EXIT_FAILURE;
 	}
@@ -59,21 +65,29 @@ int main(int argc, char *argv[]) {
 		{"nop", nop},
 		{NULL, NULL}
 	};
-	while (fgets(buffer, sizeof(buffer), file)) {
+	while (fgets(buffer, sizeof(buffer), file))
+	{
 		char *opcode = strtok(buffer, " \t\n");
-		if (opcode && opcode[0] != '#') {
-			if (strcmp(opcode, "push") == 0) {
+		if (opcode && opcode[0] != '#')
+		{
+			if (strcmp(opcode, "push") == 0)
+			{
 				char *value_str = strtok(NULL, " \t\n");
-				if (value_str && is_integer(value_str)) {
+				if (value_str && is_integer(value_str))
+				{
 					int value = atoi(value_str);
 					execute_instruction(instructions, &stack, opcode, line_number, value);
-				} else {
+				}
+				else
+				{
 					fprintf(stderr, "L%d: usage: push integer\n", line_number);
 					free_stack(stack);
 					fclose(file);
 					return EXIT_FAILURE;
 				}
-			} else {
+			}
+			else
+			{
 				execute_instruction(instructions, &stack, opcode, line_number, 0);
 			}
 		}
